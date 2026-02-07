332f03ec05577a2fb020fb5b25ef1f549df2ed8d
replyastra <officialaikannada@gmail.com>
2026-02-07 13:29:26 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
   images: {
-    unoptimized: true,
-  },
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
+
