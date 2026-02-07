cc8a3a0d12c64cbfae0ff0e57785ee82f4683361
replyastra <officialaikannada@gmail.com>
2026-02-07 18:33:56 +0530

    Update page.js

--- app/forgot-password/page.js
+++ app/forgot-password/page.js
@@ -3,7 +3,8 @@
 export const dynamic = "force-dynamic";
 
 import { useState } from "react";
-import { supabase } from "../lib/supabase";
+import { supabase } from "../../lib/supabase";
+
 
 export default function ForgotPassword() {
   const [email, setEmail] = useState("");
