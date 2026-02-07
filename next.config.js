e975614c60c325f4446aceb905667b7990198cba
replyastra <officialaikannada@gmail.com>
2026-02-07 17:42:04 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  webpack: (config) => {
+    config.cache = false; // 🔥 IMPORTANT
+    return config;
+  },
 };
 
 module.exports = nextConfig;
-
