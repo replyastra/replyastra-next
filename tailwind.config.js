3aa515f01e004e1bccd0231b947b710ced098e86
replyastra <officialaikannada@gmail.com>
2026-02-07 14:12:28 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#7c3aed",
+        secondary: "#9333ea",
+      },
+    },
+  },
+  plugins: [],
+};
