54f9e06a70fccdf7c89eef093b6c6956239abfa7
replyastra <officialaikannada@gmail.com>
2026-02-14 14:15:37 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,14 @@
 import Navbar from "./Navbar";
-import Footer from "./Footer"; // This pulls in your perfect footer
+import Footer from "./Footer";
 import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="antialiased">
         <Navbar />
-        {/* This "children" is where your Contact Page or Home Page shows up */}
-        <main>{children}</main> 
-        
-        {/* Adding this here makes it visible on ALL pages automatically */}
-        <Footer /> 
+        <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
