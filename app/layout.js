11b67e0ed66c75255f5f1b472868d33c12ddcbfc
replyastra <officialaikannada@gmail.com>
2026-02-14 13:46:09 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,18 @@
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 import "./globals.css";
-import Footer from "../components/Footer";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-[#f8fafc] overflow-x-hidden">
-        {children}
+      <body className="antialiased">
+        {/* This Navbar will now show on every page */}
+        <Navbar />
+        
+        {/* This "children" represents your Home page, Contact page, etc. */}
+        <main>{children}</main>
+        
+        {/* This Footer will now show on every page */}
         <Footer />
       </body>
     </html>
