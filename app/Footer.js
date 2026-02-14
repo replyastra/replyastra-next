7d0fef815a9843f6652d9d6f879f8386dfbcc382
replyastra <officialaikannada@gmail.com>
2026-02-14 20:56:21 +0530

    Update Footer.js

--- app/Footer.js
+++ app/Footer.js
@@ -1,7 +1,7 @@
 import { Instagram } from "lucide-react";
 export default function Footer() {
   return (
-    <footer className="bg-[#f0fdfa] border-t border-emerald-100 mt-20">
+    <footer className="bg-white border-t border-gray-100 mt-20">
       <div className="max-w-7xl mx-auto px-6 py-16">
         {/* GRID */}
         <div className="grid md:grid-cols-3 gap-12">
