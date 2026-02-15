a78b9e82ca90d21ca27c81e3750361ab57af0f04
replyastra <officialaikannada@gmail.com>
2026-02-16 00:21:20 +0530

    Update page.js

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,7 +1,8 @@
 "use client";
 
 import { useState } from "react";
-import { supabase } from "../../lib/supabase";
+import { supabase } from '@/lib/supabaseClient';
+
 import { Plus, Minus, Send } from "lucide-react";
 
 const faqs = [
