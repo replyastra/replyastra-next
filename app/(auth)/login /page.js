f3c6bac1b42db4ec1160f008d40be9e6264a75cb
replyastra <officialaikannada@gmail.com>
2026-02-16 12:21:31 +0530

    Update page.js

--- app/(auth)/login /page.js
+++ app/(auth)/login /page.js
@@ -1,8 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { supabase } from "../../lib/supabase";
-
+import { supabase } from "../../../lib/supabase";
 export default function LoginPage() {
   const [form, setForm] = useState({ email: "", password: "" });
   const [showPassword, setShowPassword] = useState(false);
