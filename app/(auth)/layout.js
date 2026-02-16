915f94008658d4b08f172cd43588fcb26e8c29f9
replyastra <officialaikannada@gmail.com>
2026-02-16 16:59:23 +0530

    Delete app/(auth) directory

--- app/(auth)/layout.js
+++
@@ -1,7 +0,0 @@
-// app/(auth)/layout.js
-// This file makes sure login, signup, dashboard pages
-// do NOT show the navbar or footer from the root layout
-
-export default function AuthLayout({ children }) {
-  return <>{children}</>;
-}
